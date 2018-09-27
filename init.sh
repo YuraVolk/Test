e1e55b868e42d6fc215c0faaaa9a4126c22900a1
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-09-27 20:24:30 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,50 +1,21 @@
 
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/test.conf
 
-#sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 
-#sudo /etc/init.d/nginx restart
-
-#sudo rm /etc/gunicorn.d/test
-
-#sudo ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-
-#sudo /etc/init.d/gunicorn restart
-
-
-
-#!/bin/bash
-
-
-
-# Install
-
-sudo pip install django-autofixture pytz
-
-sudo apt-get install -y w3m
-
-
-
-# Nginx
-
-if [ -f /etc/nginx/sites-enabled/default ]; then
-
-  sudo rm /etc/nginx/sites-enabled/default
-
-fi
-
-touch /home/box/nginx.log
-
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/ask.conf
+sudo rm /etc/nginx/sites-enabled/default
 
 sudo /etc/init.d/nginx restart
 
 
 
-# Gunicorn (ver. 17.5)
-
-touch /home/box/gunicorn.log
-
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 
 sudo /etc/init.d/gunicorn restart
+
+
+
+sudo /etc/init.d/mysql restart
+
+mysql -u root -e "CREATE DATABASE qa CHARACTER SET utf8"
+
