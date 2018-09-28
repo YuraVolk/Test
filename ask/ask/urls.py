09f9ad8792290cdbe8451cd72ee93361920fe8a9
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-09-28 08:58:11 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -1,20 +1,14 @@
 
-from django.conf.urls import patterns, include, url
-
-
+from django.conf.urls import url, include
 
 from django.contrib import admin
 
-admin.autodiscover()
 
 
-
-urlpatterns = patterns('',
-
-
+urlpatterns = [
 
     url(r'^', include('qa.urls')),
 
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', admin.site.urls),
 
-)
+]
