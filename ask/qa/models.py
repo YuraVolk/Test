10d0687a2dc87c966b1d1d4a216397a6dd2f17a1
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-09-28 08:41:07 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,29 +1,37 @@
 
-from django.db import models
+#! /usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+
 
 from django.contrib.auth.models import User
 
+from django.db import models
 
 
-
-
-# Create your models here.
 
 
 
 class Question(models.Model):
 
-    title = models.CharField(default="", max_length=1024)
+    title = models.CharField(max_length=255)    #заголовок вопроса
 
-    text = models.TextField(default="")
+    text = models.TextField(null=False)                     #полный текст вопроса
 
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True, blank=True)  #дата добавления вопроса
 
-    rating = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)     #рейтинг вопроса (число)
 
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='question_author_set')   #автор вопроса
 
-    likes = models.ManyToManyField(User, related_name="q_to_likes")
+    likes = models.ManyToManyField(User, related_name='question_like_set')        #список пользователей, поставивших "лайк"
+
+
+
+    def get_url(self):
+
+        return '/question/'+str(self.pk)+'/'
 
 
 
@@ -33,9 +41,9 @@ class Question(models.Model):
 
 
 
-    def get_url(self):
+    def __unicode__(self):
 
-        return "/question/{}/".format(self.id)
+        return self.__str__()
 
 
 
@@ -43,16 +51,22 @@ class Question(models.Model):
 
 class Answer(models.Model):
 
-    text = models.TextField(default="")
+    text = models.TextField(null=False) #текст ответа
 
-    added_at = models.DateField(null=True)
+    added_at = models.DateTimeField(auto_now_add=True, blank=True)#дата добавления ответа
 
-    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)#вопрос, к которому относится ответ
 
-    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)      #автор ответа
 
 
 
     def __str__(self):
 
         return self.text
+
+
+
+    def __unicode__(self):
+
+        return self.__str__()
